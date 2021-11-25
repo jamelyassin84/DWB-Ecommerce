aa436f976cb15ccade0175817f0695ec9484c0d1
Jamel Yassin <jamelyassin84@gmail.com>
2021-11-25 21:55:00 +0400

    Initial

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,31 +1,21 @@
-/**
- * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
- * https://reactnavigation.org/docs/getting-started
- *
- */
+import * as React from 'react'
 import { FontAwesome, FontAwesome5, Ionicons } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { RootStackParamList, RootTabParamList } from '../types'
+import { ColorSchemeName, Pressable } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Entypo } from '@expo/vector-icons'
 import {
 	NavigationContainer,
 	DefaultTheme,
 	DarkTheme
 } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import * as React from 'react'
-import { ColorSchemeName, Pressable } from 'react-native'
-
 import Colors from '../constants/Colors'
 import useColorScheme from '../hooks/useColorScheme'
 import ModalScreen from '../screens/ModalScreen'
 import NotFoundScreen from '../screens/NotFoundScreen'
 import TabOneScreen from '../screens/TabOneScreen'
 import TabTwoScreen from '../screens/TabTwoScreen'
-import {
-	RootStackParamList,
-	RootTabParamList,
-	RootTabScreenProps
-} from '../types'
-import { Entypo } from '@expo/vector-icons'
 
 export default function Navigation({
 	colorScheme
