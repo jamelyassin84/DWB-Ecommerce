62cce146731493a5994d100007b27904b90e257f
Jamel Yassin <jamelyassin84@gmail.com>
2021-11-27 12:16:11 +0400

    Calendar

--- modules/home/Calendar.tsx
+++ modules/home/Calendar.tsx
@@ -1,14 +1,20 @@
+import { FontAwesome5 } from '@expo/vector-icons'
 import React, { FC } from 'react'
-import { Text } from 'react-native'
+import { ScrollView, TouchableOpacity } from 'react-native'
 import BottomSheetHeader from '../../components/BottomSheetHeader'
 import Container from '../../components/Layout'
-import { View } from '../../components/Themed'
+import { Text, View } from '../../components/Themed'
+import Colors from '../../constants/Colors'
+import useColorScheme from '../../hooks/useColorScheme'
+import { Months } from './DummyHomeProducts'
 
 type Props = {
 	close: Function
 }
 
 const Calendar: FC<Props> = (props) => {
+	const colorScheme = useColorScheme()
+
 	return (
 		<View>
 			<BottomSheetHeader
@@ -17,6 +23,41 @@ const Calendar: FC<Props> = (props) => {
 					props.close()
 				}}
 			/>
+			<ScrollView style={{ width: '100%' }}>
+				{Months.map((month: string, index: number) => (
+					<TouchableOpacity
+						key={index}
+						style={{
+							flexDirection: 'row',
+							paddingHorizontal: 30,
+							width: '100%',
+							paddingVertical: 15
+						}}>
+						<Text
+							style={{
+								flex: 1,
+								fontSize: 16,
+								fontWeight: 'bold',
+								color:
+									index === 8 - 1
+										? Colors[colorScheme].tint
+										: '#5F6A84'
+							}}>
+							{month}
+						</Text>
+						<FontAwesome5
+							name="check"
+							size={16}
+							color={
+								index === 8 - 1
+									? Colors[colorScheme].tint
+									: 'rgba(150,150,150,.2)'
+							}
+						/>
+					</TouchableOpacity>
+				))}
+				<View style={{ marginTop: 190 }}></View>
+			</ScrollView>
 		</View>
 	)
 }
--- modules/home/DummyHomeProducts.ts
+++ modules/home/DummyHomeProducts.ts
@@ -21,3 +21,18 @@ export type HomeProductType = {
 	name: string
 	sold: number
 }
+
+export const Months: string[] = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December'
+]
